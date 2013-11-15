d0acb0566b20a18b40595b4ef0479eb98b2ea69f
5outh <bkovach13@gmail.com>
2013-11-15 12:26:09 -0500

    Moved parsing to applicative style w/ clearer errors

--- src/BFParser.hs
+++ src/BFParser.hs
@@ -1,23 +1,11 @@
 module BFParser (parseBF) where
 
 import Text.ParserCombinators.Parsec
+import Control.Applicative hiding((<|>), many)
 import Types
 
 parseBF :: String -> Either ParseError STree
 parseBF = parse ops "(unknown)"
-
-ops :: GenParser Char st STree
-ops = many $ bracket <|> operator
-
-operator :: GenParser Char st Operator
-operator = do
-  many $ noneOf "+-<>.,[]"
-  a <- oneOf "+-<>.,"
-  return (toOp a)
-
-bracket :: GenParser Char st Operator
-bracket = do
-  char '['
-  a <- ops
-  char ']'
-  return (Bracket a)
+  where ops       = many (bracket <|> operator) <?> "operations"
+        operator  = toOp <$> ( (many $ noneOf "+-<>.,[]") *> oneOf "+-<>.,") <?> "operator"
+        bracket   = Bracket  <$> (char '[' *> ops <* char ']') <?> "bracket"
